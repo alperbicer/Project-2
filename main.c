1a6a642aa08a14069834819063479bd1e62b3032
Alper Biçer <b.alper19@gmail.com>
2013-05-04 18:39:47 +0300

    Structer

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct student {
+    int    student_number;
+    char   name[31];
+    int    grade;
+    int    semester_credit;
+    float  gpa;
+};
+struct note {
+    int    course_code;
+    int    student_number;
+    int    student_point;
+    struct lesson *dugum_basi;
+    struct circular *following_number;
+    struct circular *following_point;
+    struct circular *previous_point;
+};
+struct  lesson {
+    int    course_code;
+    char   course_name[26];
+    int    credit;
+    int    student_counter;
+    float  general_gpa;
+};
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+        return 0;
 }
